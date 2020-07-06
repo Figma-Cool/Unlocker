95dcafb2d4714eb3516b87c85a7e75f77900c570
yancymin <senseslofter@gmail.com>
2020-07-07 00:29:09 +0800

    Initial commit

---
+++ code.js
@@ -0,0 +1,15 @@
+// This plugin will open a modal to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser enviroment (see documentation).
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+figma.ui.resize(600, 400);
+
+// figma.ui.onmessage = msg => {
+
+// };
