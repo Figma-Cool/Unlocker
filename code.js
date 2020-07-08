8274b5d79e7baeeca2a22a5e14e47a65a36afc6d
yancymin <senseslofter@gmail.com>
2020-07-08 16:24:31 +0800

    -

--- code.js
+++ code.js
@@ -1,25 +1,51 @@
-// This plugin will open a modal to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser enviroment (see documentation).
-// This shows the HTML page in "ui.html".
 figma.showUI(__html__);
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
+
 figma.ui.resize(600, 420);
 
-figma.on('selectionchange', () => {
-    let fName = figma.currentPage.selection[0].fontName;
-    console.log(fName)
-    figma.loadFontAsync(fName)
-    figma.currentPage.selection[0].characters = '0000';
+
+const node = figma.root.findAll()
+let isLoockArr = [];
+
+node.forEach(item => {
+    if (item.locked) {
+        let node = [item.name, item.id, item.type]
+        isLoockArr.push(node)
+    }
+    return isLoockArr
+});
+
+figma.ui.postMessage({
+    isLoockArr
 })
 
+function findLockNodes(nodeType,) {
 
-// figma.ui.onmessage = msg => {
+}
 
-// };
+figma.ui.onmessage = (msg) => {
+    if (msg.ans === 'all') {
+        figma.root.findAll().forEach(item, () => {
+            item.locked = false
+        })
+
+    }
+    console.log(msg)
+}
+
 
 // figma.off('selectionchange')
+// "DOCUMENT" |
+//     "PAGE" |
+//     "SLICE" |
+//     "FRAME" |
+//     "GROUP" |
+//     "COMPONENT" |
+//     "INSTANCE" |
+//     "BOOLEAN_OPERATION" |
+//     "VECTOR" |
+//     "STAR" |
+//     "LINE" |
+//     "ELLIPSE" |
+//     "POLYGON" |
+//     "RECTANGLE" |
+//     "TEXT"
